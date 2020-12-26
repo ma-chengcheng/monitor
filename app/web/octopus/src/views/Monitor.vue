7d329c83d728e173d67e014323a72c5ff79fa6d5
mamachengcheng <616254086@qq.com>
2020-12-26 11:09:51 +0800

    update web

--- app/web/octopus/src/views/Monitor.vue
+++ app/web/octopus/src/views/Monitor.vue
@@ -104,7 +104,7 @@ export default {
   mounted() {
 
 
-    const wsuri = "ws://127.0.0.1:8080/api/v1/ws/query";
+    const wsuri = "ws://127.0.0.1:8081/api/v1/ws/query";
     this.ws = new WebSocket(wsuri);
     this.ws.onmessage = (e) => {
       let data = JSON.parse(e.data);
